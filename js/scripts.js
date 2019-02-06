a427de2a9583a61db875a3854e4c9e086b53c092
luis Ramirez <l.r.32@hotmail.com>
2019-02-06 14:19:21 -0800

    added new features

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 $('.carousel').carousel()
+
+function initMap() {
+  // The location of Uluru
+  let uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  let map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  let marker = new google.maps.Marker({position: uluru, map: map});
+}
